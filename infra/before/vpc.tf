4d21f9d337ff091a281d304493779bfd27439054
Gustavo Melo <gustavo.melo_10@hotmail.com>
2024-03-12 12:10:23 -0300

    fix: github actions deploy

--- infra/before/vpc.tf
+++ infra/before/vpc.tf
@@ -61,12 +61,12 @@ resource "aws_internet_gateway" "gw" {
 resource "aws_route_table" "rt" {
     vpc_id = aws_vpc.vpc.id
 
-    route{
+    route {
         cidr_block = "0.0.0.0/0"
         gateway_id = aws_internet_gateway.gw.id
     }
 
-    route{
+    route {
         ipv6_cidr_block = "::/0"
         gateway_id = aws_internet_gateway.gw.id
     }
--- infra/ecs.tf
+++ infra/ecs.tf
@@ -2,29 +2,11 @@ resource "aws_ecs_cluster" "ecs" {
     name = "app_cluster"
 }
 
-resource "aws_ecs_service" "service" {
-    name = "app_service"
-    cluster = aws_ecs_cluster.ecs.arn
-    launch_type = "FARGATE"
-    enable_execute_command = true
-
-    deployment_maximum_percent = 200
-    deployment_minimum_healthy_percent = 100
-    desired_count = 1
-    task_definition = aws_ecs_task_definition.td.arn
-
-    network_configuration {
-        assign_public_ip = true
-        security_groups = [aws_security_group.sg.id]
-        subnets = [aws_subnet.sn1.id, aws_subnet.sn2.id, aws_subnet.sn3.id]
-    }
-}
-
 resource "aws_ecs_task_definition" "td" {
     container_definitions = jsonencode([
         {
             name = "app"
-            image = "${aws_ecr_repository.repo.repository_url}/${aws_ecr_repository.repo.name}"
+            image = "860605510018.dkr.ecr.sa-east-1.amazonaws.com/app_repo"
             cpu = 256
             memory = 512
             essetials = true
@@ -42,6 +24,24 @@ resource "aws_ecs_task_definition" "td" {
     cpu = "256"
     memory = "512"
     network_mode = "awsvpc"
-    task_role_arn = aws_iam_role.ecs_task_execution_role.arn
-    execution_role_arn = aws_iam_role.ecs_task_execution_role.arn
+    task_role_arn = "arn:aws:iam::860605510018:role/ecsTaskExecutionRole"
+    execution_role_arn = "arn:aws:iam::860605510018:role/ecsTaskExecutionRole"
+}
+
+resource "aws_ecs_service" "service" {
+    name = "app_service"
+    cluster = aws_ecs_cluster.ecs.arn
+    launch_type = "FARGATE"
+    enable_execute_command = true
+
+    deployment_maximum_percent = 200
+    deployment_minimum_healthy_percent = 100
+    desired_count = 1
+    task_definition = aws_ecs_task_definition.td.arn
+
+    network_configuration {
+        assign_public_ip = true
+        security_groups = ["sg-05c8e1e280f9f6b2e"]
+        subnets = ["subnet-00e0921b25f5b381c", "subnet-0ab3010574fcfe6c9", "subnet-01ebe4f621d5d98ff"]
+    }
 }
